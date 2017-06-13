8c593cf0084e54125b9cb289cf2d398fe9dca733
GVarsha <varshagunasekaran@gmail.com>
2017-06-13 11:16:48 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,1 +1,14 @@
 
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Reverse
+{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        StringBuffer s=new StringBuffer(scan.next());
+        s.reverse();
+        System.out.println(s);
+    }
+}
